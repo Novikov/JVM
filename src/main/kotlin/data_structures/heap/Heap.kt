82d6a28d72edde04f5a104a73bf353741d8e075c
Igor Novikov <igor.novikov@umbrellait.com>
2024-04-04 08:48:02 +0300

    Heap

--- src/main/kotlin/data_structures/heap/Heap.kt
+++ src/main/kotlin/data_structures/heap/Heap.kt
@@ -33,4 +33,39 @@ class Heap {
             current = parent(current)
         }
     }
+
+    private fun sinkDown(index: Int) {
+        var index = index
+        var maxIndex = index
+        while (true) {
+            val leftIndex = leftChild(index)
+            val rightIndex = rightChild(index)
+            if (leftIndex < heap.size && heap[leftIndex] > heap[maxIndex]) {
+                maxIndex = leftIndex
+            }
+            if (rightIndex < heap.size && heap[rightIndex] > heap[maxIndex]) {
+                maxIndex = rightIndex
+            }
+            index = if (maxIndex != index) {
+                swap(index, maxIndex)
+                maxIndex
+            } else {
+                return
+            }
+        }
+    }
+
+    //Обрати внимание remove без значения. Он всегда удалит максимальный элемент.
+    fun remove(): Int? {
+        if (heap.size == 0) {
+            return null
+        }
+        if (heap.size == 1) {
+            return heap.removeAt(0)
+        }
+        val maxValue = heap[0]
+        heap[0] = heap.removeAt(heap.size - 1)
+        sinkDown(0)
+        return maxValue
+    }
 }
