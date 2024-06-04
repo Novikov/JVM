171656955b9ae6da882b28890b359d74d2840ae7
Igor Novikov <igor.novikov@umbrellait.com>
2024-06-04 08:36:19 +0300

    kotlin

--- src/main/kotlin/kotlinlang/Delegates.kt
+++ src/main/kotlin/kotlinlang/Delegates.kt
@@ -1,5 +1,26 @@
 package kotlinlang
 
-fun main(){
+fun main() {
+    val initSample = InitSample()
+    println(initSample.lateinitField)
+    println(initSample.lazyField)
+}
 
+class InitSample {
+
+    /** Гарантируем что инициализируем свойство до первого обращения, иначе получим UninitializedPropertyAccessException.
+     * Тем самым уходим от nullable типов там где мы точно уверенны что придет значение, а не null.*/
+    lateinit var lateinitField: String
+
+    /** Инициализируем свойство при первом обращении. С помощью данной техники можем ускорить инициализацию класса, отложив работу по инициализации
+     * некоторых сложно вычисляемых полей на потом. Не нужно использовать для тривиальных свойств.*/
+    val lazyField by lazy { "LayField" }
+
+    private fun initName(){
+        lateinitField = "NameField"
+    }
+
+    init {
+        initName()
+    }
 }
---
+++ src/main/kotlin/kotlinlang/HardConceptions.kt
@@ -0,0 +1,5 @@
+package kotlinlang
+
+fun main(){
+    // Тут расписать такие концепции как inline/refied/infix/DSL итд
+}
