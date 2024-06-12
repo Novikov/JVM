4554cdc0579b804b7c9d5cf7a15e2a89672901e3
Igor Novikov <igor.novikov@umbrellait.com>
2024-06-12 13:20:26 +0300

    kotlin conceptions

--- src/main/kotlin/kotlin_conceptions/Delegates.kt
+++ src/main/kotlin/kotlin_conceptions/Delegates.kt
@@ -2,6 +2,8 @@ package kotlin_conceptions
 
 fun main() {
     InitSample1().printFields()
+
+
 }
 
 class InitSample1 {
@@ -27,3 +29,42 @@ class InitSample1 {
         println(lazyField)
     }
 }
+
+
+/** Делегирование методов **/
+
+/** реализация паттерна декоратор выглядит громоздко*/
+class DelegatingCollectionDecorator<T> : Collection<T> {
+    private val innerList = arrayListOf<T>()
+    override val size: Int get() = innerList.size
+    override fun contains(element: T): Boolean = innerList.contains(element)
+    override fun isEmpty(): Boolean = innerList.isEmpty()
+    override fun iterator(): Iterator<T> = innerList.iterator()
+    override fun containsAll(elements: Collection<T>): Boolean =
+        innerList.containsAll(elements)
+}
+
+
+/** Уйти от этого мы можем с помощью делегирования.
+ * Мы можем делегировать реализацию другому объекту, добавив ключевое слово by.
+ * В результате компилятор сам сгенерит имплементации методов и избавит нас от boilerplate**/
+class DelegatingCollectionMethods<T>(
+    private val innerList: Collection<T> = ArrayList()
+) : Collection<T> by innerList {
+
+    // Но некоторые методы мы можем переопределить самостоятельно.
+    override fun containsAll(elements: Collection<T>): Boolean =
+        innerList.containsAll(elements)
+}
+
+/** Делегирование свойств*/
+
+/** Эта особенность позволяет без труда реализоватьсвойства с логикой сложнее, чем хранение данных в соответствующих
+полях, без дублирования кода в каждом методе доступа. Например, свойствамогут хранить свои значения в базе данных,
+в сеансе браузера, в словаре и так далее.**/
+
+/**
+ * В основе этой особенности лежит делегирование: шаблон проектирования, согласно которому объект не сам выполняет
+ * требуемое задание, а делегирует его другому вспомогательному объекту. Такой вспомогательный объект называется делегатом.
+ * */
+
