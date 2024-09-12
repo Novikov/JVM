b533ea640fd877b6161949bc4c86bf3c951c3ba3
Novikov <nov1kov@yahoo.com>
2024-09-12 11:54:39 +0300

    length of last word

--- src/main/kotlin/cs/leetcode/easy/LengthOfLastWord.kt
+++ src/main/kotlin/cs/leetcode/easy/LengthOfLastWord.kt
@@ -1,33 +1,20 @@
 package cs.leetcode.easy
 
+
 fun main() {
     val length = lengthOfLastWord("Hello world")
+    println(length)
 }
 
-
-// Можно оптимизировать сразу возвращаясь назад справа
 fun lengthOfLastWord(s: String): Int {
-    // после слова может быть пробел
-    // критерий конца строки это отсутствие символа слова до пробела и после пробела
-    // найдя конец строки можно пройтись циклом назад по строке и полностью взять слово
-    // затем возвращаем его длину
-    val spaceSymbol = ' '
-    var lastWordIndex = 0
-    var firstWordIndex = 0
-    for (i in 0 until s.length) {
-        if (s[i] != spaceSymbol) {
-            lastWordIndex = i
-        }
-    }
-    firstWordIndex = lastWordIndex
-    for (i in lastWordIndex downTo 0) {
-        if (s[i] != spaceSymbol) {
-            firstWordIndex = i
-        } else {
-            firstWordIndex = i
+    val trimmedString = s.trim()
+    var length = 0
+    for (i in trimmedString.length - 1 downTo 0) {
+        if (trimmedString[i] != ' ') {
+            length++
+        } else if (length > 0) {
             break
         }
     }
-
-    return lastWordIndex - firstWordIndex
+    return length
 }
