4946ff479b0ea8b917cca81c6dc90405ee59fedd
Novikov <nov1kov@yahoo.com>
2024-09-13 12:07:12 +0300

    find kth largest element in array

---
+++ src/main/kotlin/cs/leetcode/easy/RemovingDuplicatesInArray.kt
@@ -0,0 +1,18 @@
+package cs.leetcode.easy
+
+fun main() {
+    val numbers = arrayOf<Int>(1, 1, 2, 3)
+    val correctLength = removeDuplicates(numbers)
+    println(correctLength)
+}
+
+fun removeDuplicates(nums: Array<Int>): Int {
+    var j = 1
+    for (i in 1 until nums.size) {
+        if (nums[i] != nums[i - 1]) {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    return j
+}
