f4b4d423765c802110adc9d67f6fbaed5d871343
Novikov <nov1kov@yahoo.com>
2024-09-19 14:28:39 +0300

    find kth largest element in array

---
+++ src/main/kotlin/cs/data_structures/list/linked_list/Program.kt
@@ -0,0 +1,16 @@
+package cs.data_structures.list.linked_list
+
+fun main(){
+    val list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    println("Before inserting: $list")
+    var middleNode = list.nodeAt(1)!!
+
+    for (i in 1..3) {
+        middleNode = list.insert(-1 * i, middleNode)
+    }
+
+    println("After inserting: $list")
+}
