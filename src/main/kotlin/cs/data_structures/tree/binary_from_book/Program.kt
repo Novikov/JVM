9cb11fe9931028db42c7c3d5a9a2d88881c4a750
Novikov <abovefortune@gmail.com>
2024-10-01 12:06:22 +0300

    algo refactor

---
+++ src/main/kotlin/cs/data_structures/tree/binary_from_book/Program.kt
@@ -0,0 +1,22 @@
+package cs.data_structures.tree.binary_from_book
+
+fun main(){
+    val zero = BinaryNode(0)
+    val one = BinaryNode(1)
+    val five = BinaryNode(5)
+    val seven = BinaryNode(7)
+    val eight = BinaryNode(8)
+    val nine = BinaryNode(9)
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+    val tree = seven
+
+    //println(seven.toString())
+
+    tree.traverseInOrder { println(it) }
+    tree.traversePreOrder { println(it) }
+    tree.traversePostOrder { println(it) }
+}
