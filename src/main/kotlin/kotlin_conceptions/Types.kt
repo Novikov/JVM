10a25a371c20a145923d5a3672f406d9f950aba2
Novikov <abovefortune@gmail.com>
2024-10-06 18:45:00 +0300

    types

---
+++ src/main/kotlin/kotlin_conceptions/Types.kt
@@ -0,0 +1,16 @@
+package kotlin_conceptions
+
+fun main(){
+
+}
+
+fun nothingExample(){
+    //Нельзя создать эксезмляр
+    //val nothing : Nothing = Nothing()
+
+    //Но можно использовать в качестве типа
+    val nothingList = listOf<Nothing>()
+
+    //Бесконечный цикл или Exception
+
+}
