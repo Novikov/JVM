24364ad04fbb2c8048cde8f9af467ed0bca02a09
Novikov <abovefortune@gmail.com>
2024-10-15 09:02:53 +0300

    Algo

---
+++ src/main/kotlin/algorithms/leetcode/tree/e_is_same_tree.kt
@@ -0,0 +1,21 @@
+package algorithms.leetcode.tree
+
+fun main(){
+
+}
+
+fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+    // Если оба дерева пустые, они идентичны
+    if (p == null && q == null) return true
+    // Если одно из деревьев пустое, а другое нет, они не идентичны
+    if (p == null || q == null) return false
+    // Сравниваем значения текущих узлов
+    if (p.`val` != q.`val`) return false
+    // Сравниваем поддеревья
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
+
+class TreeNode(var `val`: Int) {
+   var left: TreeNode? = null
+    var right: TreeNode? = null
+}
